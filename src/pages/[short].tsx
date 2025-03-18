694a947478bb8c3bcf9019565f6b7d5e8336815f
Henry Gann <henry.gann@gmail.com>
2025-03-18 13:00:23 +1300

    chore: Tidied code, updated output config.

--- src/pages/[short].tsx
+++ src/pages/[short].tsx
@@ -1,56 +1,40 @@
-import { GetServerSideProps } from 'next';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import graphqlClient from '@/util/graphqlClient';
 import { getByShort } from '@/util/graphqlRequests';
 import { SHORT_LENGTH } from '@/util/url';
 
-// Fetches long url
-const getLongUrl = async (short: string): Promise<string | null> => {
-  try {
-    const res = await graphqlClient.query({
-      query: getByShort,
-      variables: { short },
-    });
-
-    const { getByShort: { long } } = res.data;
-    return long || null;
-  } catch (error) {
-    console.error('Error fetching long URL:', error);
-    return null;
-  }
-};
-
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const short = params?.short as string;
-
-  // Checks for a valid URL else returns home
-  if (!short || short.length !== SHORT_LENGTH) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    };
-  }
-
-  const longUrl = await getLongUrl(short);
-
-  // Handle redirection
-  if (longUrl) {
-    return {
-      redirect: {
-        destination: longUrl,
-        permanent: false,
-      },
-    };
-  }
-  return {
-    redirect: {
-      destination: '/',
-      permanent: false,
-    },
-  };
-};
-
 export default function ShortPage() {
-  return null;
+  const router = useRouter();
+  const { short } = router.query;
+
+  useEffect(() => {
+    if (!router.isReady) { return; }
+
+    const redirect = async () => {
+      if (!short || typeof short !== 'string' || short.length !== SHORT_LENGTH) {
+        router.replace('/');
+        return;
+      }
+
+      try {
+        const res = await graphqlClient.query({
+          query: getByShort,
+          variables: { short },
+        });
+
+        const longUrl = res.data?.getByShort?.long || '/';
+        router.replace(longUrl);
+      } catch (error) {
+        console.log(error);
+        router.replace('/');
+      }
+    };
+
+    redirect();
+  }, [ short, router, router.isReady ]);
+
+  return (
+    <div className='h-full w-full bg-black'/>
+  );
 }
